3ce18c914dce554104975a64560170c1cbebb66a
msettles <msettles@uidaho.edu>
2012-08-27 14:47:23 -0700

    removed header display from show

--- R/methods-SffHeader.R
+++ R/methods-SffHeader.R
@@ -10,7 +10,7 @@ setMethod(show,
 	      signature="SffHeader",
 	      function(object) {
             callNextMethod()
-            sapply(header(object), function(x) cat("file:",x$filename,"; number of reads:",x$number_of_reads,"; numer of flows:",x$number_of_flows,"\n"))
+#            sapply(header(object), function(x) cat("file:",x$filename,"; number of reads:",x$number_of_reads,"; numer of flows:",x$number_of_flows,"\n"))
 })
 
 ## detail
